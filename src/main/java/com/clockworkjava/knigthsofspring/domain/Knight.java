0247bb73c70a4ebc0e31da8a7ebc38a01d1f790b
eatrzcinska <eatrzcinska@gmail.com>
2024-07-06 23:18:42 +0200

    Changing Knight class

--- src/main/java/com/clockworkjava/knigthsofspring/domain/Knight.java
+++ src/main/java/com/clockworkjava/knigthsofspring/domain/Knight.java
@@ -1,16 +1,16 @@
 package com.clockworkjava.knigthsofspring.domain;
 
+import java.util.Objects;
+
 public class Knight {
 
     private String name;
     private int age;
     private Quest quest;
 
-    public Knight(String name, int age, Quest quest) {
-        this.name = name;
-        this.age = age;
-        //this.quest = new Quest("uratuj ksiezniczke"); //tworzenie zadania przez słówko new w konstruktorze
-        this.quest = quest;
+    public Knight() {
+        this.name = "Lancelot";
+        this.age = 25;
     }
 
     public Knight(String name, int age) {
@@ -18,11 +18,42 @@ public class Knight {
         this.age = age;
     }
 
-    public void setQuest(Quest quest){
+    public void setQuest(Quest quest) {
+        System.out.println("Setting the quest for the knight");
         this.quest = quest;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
     public String toString() {
         return "Rycerz o imieniu " + name + " wiek: " + age + " zadanie: " + quest;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(obj == null || obj.getClass() != getClass()) return false;
+        Knight knight = (Knight) obj;
+        return knight.age == age;
     }
 }
